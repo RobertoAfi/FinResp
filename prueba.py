a729d15d3b1ee42bae12e31abed87c81f5e88c1c
RobertoAfi <98113327+RobertoAfi@users.noreply.github.com>
2022-01-24 15:39:27 +0100

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title('Repositorio FinResp')
 
-data = pd.read_excel("tabla.xlsx")
+data = pd.read_excel("Tabla.xlsx")
 
 x = st.slider('x')
 st.write(x, 'squared is', x * x)
